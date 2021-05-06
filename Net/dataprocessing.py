5381562078611c4cf054b60d95487746da23596b
Domenico Dell'Olio <zeito99@gmail.com>
2021-05-06 10:35:11 +0200

    Revisionato codice fino a refactor_labels

--- Net/dataprocessing.py
+++ Net/dataprocessing.py
@@ -11,6 +11,9 @@ def read_mat(path, label):
     """
     function returning an image
     read from a .mat file in "path"
+    :param path: string containing the path of the image
+    :param label: label corresponding to the image in che .mat dictionary
+    :return: a 3d numpy array containing the image
     """
     mat = sio.loadmat(path)
     return mat[label]
@@ -20,6 +23,9 @@ def minmax_pair(img1, img2):
     """
     function rescaling the pair of images with min-max scaling
     it assumes img1 and img2 are same-sized 2-dim arrays with shape (height x width,spectral bands)
+    :param img1: 2-dim numpy array containing pixels of the first images
+    :param img2: 2-dim numpy array containing pixels of the second images
+    :return: two 2-dim numpy array containing the rescaled pixel values
     """
     # concat
     toscale = np.concatenate((img1, img2), axis=0)
@@ -66,15 +72,15 @@ def refactor_labels(labels, conf_section):
     return labels
 
 
-def generate_set(img1, img2, labels, unknown_label):
+def generate_set(img1, img2, labels, keep_unlabeled):
     """
     Takes the two images, the respective labels and the value of the label for the unlabeled pixels and returns
     a 2-dim array containing the pair of pixel before/after and a 1-dim array containing the label for the pair.
-    Every unlabeled pixel pair is removed.
+    The unlabeled pixels (marked as UNKNOWN_LABEL) can be removed, if necessary
     :param img1: a 2-dim numpy array of shape (height x width, spectral bands)
     :param img2: a 2-dim numpy array of shape (height x width, spectral bands) - the same shape as img1
     :param labels: a 1-dim numpy array containing a label for each pair
-    :param unknown_label: the value of the "unlabeled pixels" label
+    :param keep_unlabeled: a Boolean flag. If True, keeps all the unlabeled pair of pixels (for testing purposes)
     :return: a 3-dim numpy array of pixel pairs of shape (height x width, 2, spectral bands) and the "labels" array
             without "unknown" labels
     """
@@ -82,7 +88,7 @@ def generate_set(img1, img2, labels, unknown_label):
     label_list = []
     # TODO: check if there's a more efficient method
     for i in range(0, img1.shape[0]):
-        if labels[i] != unknown_label:
+        if keep_unlabeled or labels[i] != config.UNKNOWN_LABEL:
             pair_list.append([img1[i], img2[i]])
             label_list.append(labels[i])
     return np.asarray(pair_list), np.asarray(label_list)
@@ -100,6 +106,7 @@ def load_dataset(name, conf):
         - the second image
         - the labels
     """
+    #TODO: modificare nomi in modo da avere lo stesso per ogni cartella
     if name not in conf.sections():
         raise ValueError(name + " dataset not available")
 
@@ -144,17 +151,18 @@ def load_label(path, conf_section):
         raise NotImplementedError("Error: CANNOT LOAD LABEL FILE FORMAT")
 
 
-def preprocessing(limgA, limgB, llabel, conf_section):
+def preprocessing(limgA, limgB, llabel, conf_section, keep_unlabeled):
     """
     Function that takes in input a pair of images and a pixel-wise label map and returns
     an array of minmaxscaled pixel pairs and an array of refactored labels.
-    All the pixel pairs labeled as "unknown" are discarded
+    All the pixel pairs labeled as "unknown" can be discarded
     :param limgA: a list of 3-dim numpy array of shape (height, width, spectral bands) from where the first pixel of the pair
                 will be extracted
     :param limgB: a list of 3-dim numpy array of shape (height, width, spectral bands) from where the second pixel of the pair
                 will be extracted
     :param llabel: a list of 2-dim array of shape (height, width) containing the label for each pixel pair.
     :param conf_section: a config parser section instance containing info obout the dataset
+    :param keep_unlabeled: a Boolean flag. If True, keeps all the unlabeled pair of pixels (for testing purposes)
     :return:a list containing:
             - an array containing the labeled pairs of pixels from the images
             - an array containing the labels of the respective pairs
@@ -185,6 +193,6 @@ def preprocessing(limgA, limgB, llabel, conf_section):
 
     # pair generation
     print("Info: STARTING PAIRING PROCEDURE...")
-    pairs, label = generate_set(imgA, imgB, label, config.UNKNOWN_LABEL)
+    pairs, label = generate_set(imgA, imgB, label, keep_unlabeled)
 
     return pairs, label
