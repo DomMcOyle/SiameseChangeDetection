2e9d9ee60ffcfa0b7b52d0ba0f1089573aba1e4b
Domenico Dell'Olio <zeito99@gmail.com>
2021-05-08 11:44:53 +0200

    Sistemato main

--- Net/main.py
+++ Net/main.py
@@ -8,7 +8,7 @@ import config
 import configparser
 
 if __name__ == '__main__':
-    dataset_name = "BAY AREA"
+    dataset_name = "SANTA BARBARA"
     model_name = "BASAM6410.h5"
     distance = s.SAM
 
@@ -35,19 +35,21 @@ if __name__ == '__main__':
         trained_model = s.siamese_model(first_img[0][0][0].shape, int(parser[dataset_name].get("FirstLayerNeurons")), distance)
         trained_model.load_weights(config.MODEL_SAVE_PATH + model_name)
 
-        for i in range(0, len(first_img)):
-            # preprocessing
-            x_test, y_test = dp.preprocessing([first_img[i]], [second_img[i]], [labels[i]], parser[dataset_name], True)
 
-            # prediction
-            distances = trained_model.predict([x_test[:, 0], x_test[:, 1]])
+        # preprocessing
+        x_test, y_test = dp.preprocessing(first_img, second_img, labels, parser[dataset_name], True)
 
-            # converting distances into labels
+        # prediction
+        distances = trained_model.predict([x_test[:, 0], x_test[:, 1]])
 
-            prediction = np.where(distances.ravel() < 0.5, config.UNCHANGED_LABEL, config.CHANGED_LABEL)
+        # converting distances into labels
+        prediction = np.where(distances.ravel() < 0.5, config.UNCHANGED_LABEL, config.CHANGED_LABEL)
 
+        i = 0
+        for lab in labels:
+            img = prediction[i:i+lab.size]
             # confusion matrix
-            cm = skm.confusion_matrix(y_test, prediction, labels=[config.CHANGED_LABEL, config.UNCHANGED_LABEL])
+            cm = skm.confusion_matrix(y_test, img, labels=[config.CHANGED_LABEL, config.UNCHANGED_LABEL])
 
             # printing the metrics
             metrics = s.get_metrics(cm)
@@ -61,5 +63,6 @@ if __name__ == '__main__':
             file.write("CONFUSION MATRIX:\n" + str(cm))
             file.close()
 
-            fig = s.plot_maps(prediction, dp.refactor_labels(np.asarray(labels[i]), parser[dataset_name]))
+            fig = s.plot_maps(img, dp.refactor_labels(lab, parser[dataset_name]))
             fig.savefig(config.STAT_PATH + dataset_name+"_on_"+model_name+".png", dpi=300, bbox_inches='tight')
+            i = i + lab.size
